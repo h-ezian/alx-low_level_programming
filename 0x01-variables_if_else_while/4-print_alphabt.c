3871be5d743a0f1796132dc9074860cb66851792
EZIAN Hippolyte <ezianhippolyte@gmail.com>
2023-10-08 07:53:54 -0700

    print alphabets without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Description: 'C program to print the alphabet in lower case'
+ *Return: Always 0
+ */
+int main(void)
+{
+int alphabets[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+		     'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+		     's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+int i = 0;
+while (i < 26)
+{
+if (alphabets[i] == 'q' || alphabets[i] == 'e')
+continue;
+
+putchar(alphabets[i]);
+i++;
+}
+putchar('\n');
+return (0);
+}
