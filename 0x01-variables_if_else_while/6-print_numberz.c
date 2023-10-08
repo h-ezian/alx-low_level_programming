16bb440057c9d9e6f7943f11eb57d440f53d1896
EZIAN Hippolyte <ezianhippolyte@gmail.com>
2023-10-08 08:06:43 -0700

    print numbersz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Description: 'C program to print number from 0 to 9'
+ *Return: Always 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
