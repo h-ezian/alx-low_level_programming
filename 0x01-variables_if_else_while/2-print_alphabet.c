73a2a722afaf80cf1ef6815c0ac40755f211ccd5
EZIAN Hippolyte <ezianhippolyte@gmail.com>
2023-10-08 07:36:31 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Description: 'C program to print the alphabet in lower case'
+ *Return: Always 0
+ */
+int main(void)
+{
+int alphabets[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+		     'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+		     's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+int i = 0;
+while (i < 26)
+{
+putchar(alphabets[i]);
+i++;
+}
+return (0);
+}
