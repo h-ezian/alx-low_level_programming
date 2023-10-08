0afac2ba717626b160f66dc7e1bab21bc3e3b29a
EZIAN Hippolyte <ezianhippolyte@gmail.com>
2023-10-08 11:36:48 -0700

    print base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int main(void)
 int i;
 char hex[16] = {'0', '1', '2', '3', '4', '5', '6',
 '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-for (i = 0; i <= 14; i++)
+for (i = 0; i <= 15; i++)
 {
 putchar(hex[i]);
 }
