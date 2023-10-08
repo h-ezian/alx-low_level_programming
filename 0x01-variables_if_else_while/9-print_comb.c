bec0d769f49fe34b01b8e0e8b127e92d3a1d46f8
EZIAN Hippolyte <ezianhippolyte@gmail.com>
2023-10-08 11:56:28 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Description: 'C program to print number from 0 to 9'
+ *Return: Always 0
+ */
+int main(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+if (i != '9')
+putchar(',');
+}
+putchar('\n');
+return (0);
+}
