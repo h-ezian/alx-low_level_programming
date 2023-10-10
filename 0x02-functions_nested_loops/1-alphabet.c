f4a23eee4a65b9712f21d199db08604e921c88bc
EZIAN Hippolyte <ezianhippolyte@gmail.com>
2023-10-10 05:10:35 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Program Entry point
+ *Description: 'C program to print alphabet'
+ *Return: Always 0
+ */
+int main(void)
+{
+int i;
+for(i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+return (0);
+}
