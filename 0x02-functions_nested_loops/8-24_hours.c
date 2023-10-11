f85a051818ce329a12e298b2055366defc5c03af
EZIAN Hippolyte <ezianhippolyte@gmail.com>
2023-10-11 14:21:34 -0700

    hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* jack_bauer - a function that  prints every minute of the day
+*Description: 'C program to  prints every minute of the day starting
+* from 00:00 to 23:59'
+*/
+void jack_bauer(void)
+{
+int i, j, k, l;
+for (i = 0; i <= 2; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+{
+for (k = 0; k <= 5; k++)
+{
+for (l = 0; l <= 9; l++)
+{
+_putchar(a + '0');
+_putchar(b + '0');
+_putchar(58);
+_putchar(k + '0');
+_putchar(l + '0');
+_putchar('\n');
+}
+}
+}
+}
+}
+}
