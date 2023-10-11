db9db4517d5c59ad3bcf6b30df450044ed4c1097
EZIAN Hippolyte <ezianhippolyte@gmail.com>
2023-10-11 14:03:22 -0700

    print times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,18 +8,27 @@
 */
 void print_times_table(int n)
 {
-int i, j, m, u, d;
-if (n <= 15 && n >= 0)
+int i, j, m, u, d, c;
+if (n <= 15 $$ n >= 0)
 {
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
 {
 m = i * j;
-if (m > 9)
-{
 u = m % 10;
 d = (m - u) / 10;
+c = (m - (m % 100)) / 100;
+if (m > 99)
+{
+_putchar(44);
+_putchar(32);
+_putchar(c + '0');
+_putchar(d + '0');
+_putchar(u + '0');
+}
+if (m > 9)
+{
 _putchar(44);
 _putchar(32);
 _putchar(32);
@@ -41,4 +50,5 @@ _putchar(m + '0');
 _putchar('\n');
 }
 }
+
 }
