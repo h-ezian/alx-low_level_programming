e9ada5798c866a83779a278111c3747b58808d27
EZIAN Hippolyte <ezianhippolyte@gmail.com>
2023-10-12 13:57:21 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_numbers - function that that multiplies two integers.
+*Description: 'function that that multiplies two integers.'
+*/
+void print_numbers(void)
+{
+int i = 48;
+while (i <= 57)
+{
+_putchar(i);
+}
+_puchar('\n')
+}
