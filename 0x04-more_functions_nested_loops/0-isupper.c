8b7ed55ed0debf5a99132038a8aebcd6b29801c0
EZIAN Hippolyte <ezianhippolyte@gmail.com>
2023-10-12 13:15:12 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _isupper - function that checks for uppercase character.
+*Description: 'function that checks for uppercase character'
+*@c: staring
+*Return: 1 if c is uppercase 0 otherwise
+*/
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
