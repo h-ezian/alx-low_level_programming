eda47d575372b5df4e7a9e9ede72495619fd4a63
EZIAN Hippolyte <ezianhippolyte@gmail.com>
2023-10-13 15:24:01 -0700

    print numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,16 +5,14 @@
 */
 void print_most_numbers(void)
 {
-int i = 48;
-while (i < 57)
+int i;
+for (i = 48; i <= 57; i++)
 {
 if (i == 50 || i == 52)
 {
-i++;
 continue;
 }
 _putchar(i);
-i++;
 }
 _putchar('\n');
 }
