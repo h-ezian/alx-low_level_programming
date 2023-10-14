fb4ac7993cff694beb4e56995370569178f5ee0c
EZIAN Hippolyte <ezianhippolyte@gmail.com>
2023-10-13 18:32:14 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,23 +12,26 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-
 if (i % 3 == 0 && i % 5 != 0)
 {
-printf("Fizz ");
-continue;
+printf("Fizz");
 }
 else if (i % 3 != 0 && i % 5 == 0)
 {
-printf("Buzz ");
-continue;
+printf("Buzz");
 }
 else if (i % 3 == 0 && i % 5 == 0)
 {
-printf("FizzBuzz ");
-continue;
+printf("FizzBuzz");
 }
-printf("%d ", i);
+else
+{
+printf("%d", i);
+}
+if (i != 100)
+{
+printf(" ");
+}
 }
 printf("\n");
 return (0);
