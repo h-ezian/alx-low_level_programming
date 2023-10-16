8bcbd73252c7aa017f243c3e67f86985a37cd342
EZIAN Hippolyte <ezianhippolyte@gmail.com>
2023-10-16 16:20:35 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <string.h>
 /**
-* swap_int - function  that returns the length of a string.
+* _strlen - function  that returns the length of a string.
 *Description: 'function  that returns the length of a string'
 *@s: param1
 *Return: the length of a string
