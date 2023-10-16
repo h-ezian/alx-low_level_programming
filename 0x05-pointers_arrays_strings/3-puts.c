7b89959f9c2cacd8416685b96af5ebaca080fb6b
EZIAN Hippolyte <ezianhippolyte@gmail.com>
2023-10-16 16:43:21 -0700

    put

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+* _puts - function  that prints a string, followed by a new line, to stdout.
+*Description: 'function  that prints a string, followed by a new line, to stdout'
+*@s: param
+*/
+void _puts(char *str)
+{
+puts(str);
+}
