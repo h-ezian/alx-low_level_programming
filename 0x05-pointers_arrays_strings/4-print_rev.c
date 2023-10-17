4ed0a6ad1f020e6dca3285e6b1865622281ac1b0
EZIAN Hippolyte <ezianhippolyte@gmail.com>
2023-10-16 17:04:26 -0700

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+* print_rev - function  that prints a string, in reverse.
+*Description: 'function  that prints a string, in reverse'
+*@s: param
+*/
+void print_rev(char *s)
+{
+strrev(s);
+}
