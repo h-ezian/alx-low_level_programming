689baf1ccdad513a177f18f0fce5d12129897818
EZIAN Hippolyte <ezianhippolyte@gmail.com>
2023-10-17 16:18:51 -0700

    Update

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,14 +10,7 @@ void puts_half(char *str)
 {
 int i, len;
 len = strlen(str);
-if (len % 2 == 0)
-{
 i = strlen(str) / 2;
-}
-else
-{
-i = (strlen(str) - 2) / 2;
-}
 for (; i < len; i++)
 {
 _putchar(str[i]);
