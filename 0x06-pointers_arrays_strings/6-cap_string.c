892a0bc54a6bc80b6495e4ee648b9ad9692774cf
EZIAN Hippolyte <ezianhippolyte@gmail.com>
2023-10-20 12:13:36 -0700

    cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,42 +8,33 @@
 */
 char *cap_string(char *str)
 {
-char *min = "abcdefghijklmnopqrstuvwxyz";
-char *maj = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char *min = "abcdefghijklmnopqrstuvwxyz0123456789";
+char *maj = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 int sep[] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
-int i, j, upperFlag, k;
-upperFlag = 0;
+int i, j, toUpper, k;
+toUpper = 1;
 i = 0;
 while (str[i] != '\0')
 {
-if ((str[i] >= 65 && str[i] <= 90) || (str[i] >= 48 && str[i] <= 57))
+if (toUpper == 0)
 {
-upperFlag = 0;
-}
-if (upperFlag == 1)
-{
-if (str[i] >= 98 && str[i] <= 122)
-{
-for (j = 0; j < 26; j++)
-{
-if (str[i] == min[j])
-{
-str[i] = maj[j];
-upperFlag = 0;
-break;
-}
-}
-}
-}
-k = 0;
-while (k < 13)
+for (k = 0; k < 13; k++)
 {
 if (str[i] == sep[k])
 {
-upperFlag = 1;
+toUpper = 1;
 break;
 }
-k++;
+}
+}
+for (j = 0; j < 36; j++)
+{
+if ((str[i] == min[j]  || str[i] == maj[j]) && toUpper == 1)
+{
+str[i] = maj[j];
+toUpper = 0;
+break;
+}
 }
 i++;
 }
